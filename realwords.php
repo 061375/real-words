b4824127780f57d4f1ac7f11020bef069ed026fa
Jeremy Heminger <jeremy@geomail.info>
2016-12-16 12:51:10 -0800

    strip html and add pharma

--- realwords.php
+++ realwords.php
@@ -1,10 +1,7 @@
 <?php
 /**
  * Real Words Algorithm
- * @author Jeremy Heminger c/o Geographics http://www.mygeographics.info -> http://www.geocreativelab.com
- *
- * Simple algorithm to devine the legitimacy of a submitted email from a form
- * 
+ * @author Jeremy Heminger c/o Geographics
  * */
 
  /**
@@ -12,20 +9,25 @@
   * @param string $i
   * */
 function real_words($i) {
-
+    
+    // remove any HTML
+    $i = strip_tags($i);
+    
+    if(false === pharma($i))return false;
+    
     // maximum words we will allow before we say that this email is likely spam
     $threshhold = 3;
-
+    
     $score = 0;
-
+    
     // put all the words into an array to loop through
     $ws = explode(" ",$i);
-
+    
     // loop all the words
     foreach($ws as $w) {
-        if(false === real_word($w))$score++;
+        if(false === real_word($w))$score++;    
     }
-
+    
     // check the obverall score and return a verdict
     if($score > $threshhold)return false;
     return true;
@@ -35,38 +37,55 @@ function real_words($i) {
   * @param string $i
   * */
 function real_word( $i ) {
-
-    $vowels = 'aeiouy'; // lets assume sometimes y is always y
-
+    
+    $vowels = 'aeiouy';
+    
     $consonants = 'bcdfghjklmnpqrstvwxz';
-
+    
     $score = 0;
-
+    
     $overallscore = 0;
-
+    
+    $finalverdict = 0;
+    
     // loop through all the letters in the word
     for($x=0; $x<strlen($i); $x++) {
         // get the letter at position x
         $l = substr($i,$x,1);
-
+        
         // if we find a vowel...clear the scrore
         if(strpos($vowels,strtolower($l)) !== false)$score = 0;
-
+        
         // if we find a consonant...how many have we found in a row?
         if(strpos($consonants,strtolower($l)) !== false)$score++;
 
-        // increment an overall score relevant to how much it is clearly not a word
         if($score == 3)$overallscore+=5;
         if($score == 4)$overallscore+=15;
         if($score == 5)$overallscore+=30;
         if($score >= 6)$overallscore+=60;
     }
-
+    
     // based on the number of consectutive consonants in a row and the length of the word
-    $finalverdict = $overallscore / strlen($i);
+    if(strlen($i) > 0)$finalverdict = $overallscore / strlen($i);
 
     // a final verdict is rendered if more than 15% of the word is garbage
     if($finalverdict > 15)return false;
+    return true;
+}
+/**
+ * string comparison looking for spam pharma emails
+ * */
+function pharma($i) {
+    $count = 0;
+    $max = 3;
+    $pharma = array('cialis','viagra','levitra','propecia','amoxicilina','lithium','zoloft','paxil','valtrex','amoxicillin','fluoxetine');
+    foreach($pharma as $p) {
+        if(strpos(strtolower($i),$p) !== false) {
+            $count++;
+        }
+        if($count > $max)continue;
+    }
+    if($count > $max)return false;
     return true;
 }
 ?>
