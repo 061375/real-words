b01e57c9254f668a453807ab2625c3f6c93d8c2e
Jeremy Heminger <jeremy@geomail.info>
2017-01-02 08:46:20 -0800

    make class

--- realwords.php
+++ realwords.php
@@ -3,89 +3,95 @@
  * Real Words Algorithm
  * @author Jeremy Heminger c/o Geographics
  * */
-
- /**
-  * loops through words and makes a final judgement if the email is spam
-  * @param string $i
-  * */
-function real_words($i) {
-    
-    // remove any HTML
-    $i = strip_tags($i);
-    
-    if(false === pharma($i))return false;
-    
-    // maximum words we will allow before we say that this email is likely spam
-    $threshhold = 3;
-    
-    $score = 0;
-    
-    // put all the words into an array to loop through
-    $ws = explode(" ",$i);
-    
-    // loop all the words
-    foreach($ws as $w) {
-        if(false === real_word($w))$score++;    
-    }
-    
-    // check the obverall score and return a verdict
-    if($score > $threshhold)return false;
-    return true;
-}
- /**
-  * loops through letters in the word to devine if the word is real
-  * @param string $i
-  * */
-function real_word( $i ) {
-    
-    $vowels = 'aeiouy';
-    
-    $consonants = 'bcdfghjklmnpqrstvwxz';
-    
-    $score = 0;
-    
-    $overallscore = 0;
-    
-    $finalverdict = 0;
-    
-    // loop through all the letters in the word
-    for($x=0; $x<strlen($i); $x++) {
-        // get the letter at position x
-        $l = substr($i,$x,1);
+class JH_RealWords
+{
+    /**
+     * loops through words and makes a final judgement if the email is spam
+     * @param string $i
+     * */
+    public static function real_words($i) {
         
-        // if we find a vowel...clear the scrore
-        if(strpos($vowels,strtolower($l)) !== false)$score = 0;
+        // remove any HTML
+        $i = strip_tags($i);
         
-        // if we find a consonant...how many have we found in a row?
-        if(strpos($consonants,strtolower($l)) !== false)$score++;
+        if(false === JH_RealWords::pharma($i))return false;
+        
+        // maximum words we will allow before we say that this email is likely spam
+        $threshhold = 3;
+        
+        $score = 0;
+        
+        // put all the words into an array to loop through
+        $ws = explode(" ",$i);
+        
 
-        if($score == 3)$overallscore+=5;
-        if($score == 4)$overallscore+=15;
-        if($score == 5)$overallscore+=30;
-        if($score >= 6)$overallscore+=60;
-    }
-    
-    // based on the number of consectutive consonants in a row and the length of the word
-    if(strlen($i) > 0)$finalverdict = $overallscore / strlen($i);
-
-    // a final verdict is rendered if more than 15% of the word is garbage
-    if($finalverdict > 15)return false;
-    return true;
-}
-/**
- * string comparison looking for spam pharma emails
- * */
-function pharma($i) {
-    $count = 0;
-    $max = 3;
-    $pharma = array('cialis','viagra','levitra','propecia','amoxicilina','lithium','zoloft','paxil','valtrex','amoxicillin','fluoxetine');
-    foreach($pharma as $p) {
-        if(strpos(strtolower($i),$p) !== false) {
-            $count++;
+        $wc = count($ws);
+        
+        
+        // loop all the words
+        foreach($ws as $w) {
+            if(false === JH_RealWords::real_word($w))$score++;    
         }
-        if($count > $max)continue;
+        
+        // check the obverall score and return a verdict
+        if($score > $threshhold)return false;
+        return true;
     }
-    if($count > $max)return false;
-    return true;
+    /**
+     * loops through letters in the word to devine if the word is real
+     * @param string $i
+     * */
+    public static function real_word( $i ) {
+    
+        $vowels = 'aeiouy';
+        
+        $consonants = 'bcdfghjklmnpqrstvwxz';
+        
+        $score = 0;
+        
+        $overallscore = 0;
+        
+        $finalverdict = 0;
+        
+        // loop through all the letters in the word
+        for($x=0; $x<strlen($i); $x++) {
+            // get the letter at position x
+            $l = substr($i,$x,1);
+            
+            // if we find a vowel...clear the scrore
+            if(strpos($vowels,strtolower($l)) !== false)$score = 0;
+            
+            // if we find a consonant...how many have we found in a row?
+            if(strpos($consonants,strtolower($l)) !== false)$score++;
+    
+            if($score == 3)$overallscore+=5;
+            if($score == 4)$overallscore+=15;
+            if($score == 5)$overallscore+=30;
+            if($score >= 6)$overallscore+=60;
+        }
+        
+        // based on the number of consectutive consonants in a row and the length of the word
+        if(strlen($i) > 0)$finalverdict = $overallscore / strlen($i);
+    
+        // a final verdict is rendered if more than 15% of the word is garbage
+        if($finalverdict > 15)return false;
+        return true;
+    }
+    /**
+     * string comparison looking for spam pharma emails
+     * */
+    public static function pharma($i) {
+        $count = 0;
+        $max = 3;
+        $pharma = array('cialis','viagra','levitra','propecia','amoxicilina','lithium','zoloft','paxil','valtrex','amoxicillin','fluoxetine');
+        foreach($pharma as $p) {
+            if(strpos(strtolower($i),$p) !== false) {
+                $count++;
+            }
+            if($count > $max)continue;
+        }
+        if($count > $max)return false;
+        return true;
+    }
 }
 ?>
--- test.php
+++ test.php
@@ -24,7 +24,7 @@ reference 1: XB2V5G  <a href="http://uzqqxswchpeq.com/">uzqqxswchpeq</a>, [url=h
 reference 2: XB2V5G  <a href="http://uzqqxswchpeq.com/">uzqqxswchpeq</a>, [url=http://stuzfbwxourz.com/]stuzfbwxourz[/url], [link=http://ozkhhjgnnaxy.com/]ozkhhjgnnaxy[/link], http://gfsdvzywzann.com/
 Special skills or qualifications: XB2V5G  <a href="http://uzqqxswchpeq.com/">uzqqxswchpeq</a>, [url=http://stuzfbwxourz.com/]stuzfbwxourz[/url], [link=http://ozkhhjgnnaxy.com/]ozkhhjgnnaxy[/link], http://gfsdvzywzann.com/';
 
-$r = real_words($t);
+$r = JH_RealWords::real_words($t);
 
 if(false === $r){print "SPAM";}else{print "NOT SPAM";}
 
@@ -42,7 +42,7 @@ print "\n";
 $t = "How did you hear about us?: Cephalexin Dosage Chart For Cats  <a href=http://exdrugs.com>viagra online</a> Amoxicillin Pill Identifier Online Viagra Chewable Mediciations Without Perscription Buspar Discontinued  <a href=http://bmpha.com>levitra et jus de pamplemousse</a> Bentyl Real No Prescription Acquistare Levitra On Line Di Cialis Generic Yagara Kamagra Jelly 100mg Tolosa  <a href=http://achatpriligyfrance.com>priligy dosage</a> Nuevo Hora Para Tomar Propecia Cialis Generique Effet Secondaire Kamagra Soft Tabs 100mg  <a href=http://shopbyrxbox.com>viagra</a> Cialis Forum Cialis Pharmacy ajanta Kamagra Oral Jelly Acheter Kamagra Clermont Zithromax Other Names  <a href=http://corzide.com>viagra</a> Levitra 20mg 8 Stuck Preisvergleich Buy Generic Celebrex No Prescription Pastillas Priligy Precio Viagra With Dapoxetine Tetraciclina  <a href=http://gammam.net>where can i order levitra</a> Order Cost Of Propecia Cialis Verona Cialis Inconvenients Viagra 110 Mg  <a href=http://ilfrc.com>genericos del viagra</a> Commander Cialis Tadalafil Buy Valtrexin Usa Cialis Levitra Ou Viagra  <a href=http://euhomme.com>cialis price</a> Amoxil Effets Sildenafil And Dapoxetine In India Zithromax And Lyme Disease Comparaison Cialis Et Levitra Pharmacycustomercare  <a href=http://vkblue.com>buy strattera online usa pharmacy</a> Purchase Tadalafil Online Levitra En Horaire Propecia Dosis Calvicie  <a href=http://byrxboxshop.com>viagra</a> Cephalexin Can Cause Skin Rash Cialis Combien De Temps Avant
 Questions or comments: Cephalexin Dosage Chart For Cats  <a href=http://exdrugs.com>viagra online</a> Amoxicillin Pill Identifier Online Viagra Chewable Mediciations Without Perscription Buspar Discontinued  <a href=http://bmpha.com>levitra et jus de pamplemousse</a> Bentyl Real No Prescription Acquistare Levitra On Line Di Cialis Generic Yagara Kamagra Jelly 100mg Tolosa  <a href=http://achatpriligyfrance.com>priligy dosage</a> Nuevo Hora Para Tomar Propecia Cialis Generique Effet Secondaire Kamagra Soft Tabs 100mg  <a href=http://shopbyrxbox.com>viagra</a> Cialis Forum Cialis Pharmacy ajanta Kamagra Oral Jelly Acheter Kamagra Clermont Zithromax Other Names  <a href=http://corzide.com>viagra</a> Levitra 20mg 8 Stuck Preisvergleich Buy Generic Celebrex No Prescription Pastillas Priligy Precio Viagra With Dapoxetine Tetraciclina  <a href=http://gammam.net>where can i order levitra</a> Order Cost Of Propecia Cialis Verona Cialis Inconvenients Viagra 110 Mg  <a href=http://ilfrc.com>genericos del viagra</a> Commander Cialis Tadalafil Buy Valtrexin Usa Cialis Levitra Ou Viagra  <a href=http://euhomme.com>cialis price</a> Amoxil Effets Sildenafil And Dapoxetine In India Zithromax And Lyme Disease Comparaison Cialis Et Levitra Pharmacycustomercare  <a href=http://vkblue.com>buy strattera online usa pharmacy</a> Purchase Tadalafil Online Levitra En Horaire Propecia Dosis Calvicie  <a href=http://byrxboxshop.com>viagra</a> Cephalexin Can Cause Skin Rash Cialis Combien De Temps Avant";
 
-$r = real_words($t);
+$r = JH_RealWords::real_words($t);
 
 if(false === $r){print "SPAM";}else{print "NOT SPAM";}
 
@@ -61,7 +61,7 @@ print "\n";
 //TEST FOR NOT SPAM
 $t = "This is a legitimate statement that has words that might through the test off like chrome, Archchronicler, catchphrase, eschscholtzia, latchstring, lengthsman, and postphthisic";
 
-$r = real_words($t);
+$r = JH_RealWords::real_words($t);
 
 if(false === $r){print "SPAM";}else{print "NOT SPAM";}
 
